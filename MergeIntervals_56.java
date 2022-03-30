4aafe61d62d564210a42825b84b38c79718b6b7f
Jatan Patel <jatanpatel92@users.noreply.github.com>
2022-03-29 19:24:19 -0500

    Update MergeIntervals_56.java

--- MergeIntervals_56.java
+++ MergeIntervals_56.java
@@ -73,3 +73,30 @@ class Solution {
         }
     }
 }
+/*
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> list = new LinkedList<>();
+        Arrays.sort(intervals, (a, b)->Integer.compare(a[0], b[0]));
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i=1; i<intervals.length; i++){
+            if(intervals[i][0]<=end){
+                end = Math.max(intervals[i][1], end);
+            }
+            else{
+                list.add(new int[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+        }
+        list.add(new int[]{start, end});
+        int[][] result = new int[list.size()][2];
+        int count = 0;
+        for(int[] interval : list){
+            result[count++] = interval;
+        }
+        return result;
+    }
+}
+*/
